3cfcac04c4e8d959e03a2c23d69d543c00798e6e
digitaltomer <tomamadin@gmail.com>
2022-09-08 15:35:41 -0700

    5 printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints with proper grammar
+ *
+ * Return Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcomes is a piece of art,\n");
+	return (0);
+}
