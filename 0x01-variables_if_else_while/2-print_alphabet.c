d31aac9c1854167b6914a20cc3451ca2c49f3c5b
digitaltomer <tomamadin@gmail.com>
2022-09-09 11:30:35 -0700

    2 print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	putchar("lowercase characters : \n");
+	for (ch = 'a'; ch <= 'z'; ch++)
+{
+	return (0);
+}
