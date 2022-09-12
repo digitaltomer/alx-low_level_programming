bf7fb47f8670dff5b10fb92148ebd23b0f644c8e
digitaltomer <tomamadin@gmail.com>
2022-09-12 14:00:39 -0700

    100 print

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -19,11 +19,12 @@ int main(void)
 
 				putchar(x);
 				putchar(y);
+
 				if (x == '8' && y == '9')
 					continue;
 
-					putchar(',');
-					putchar(' ');
+				putchar(',');
+				putchar(' ');
 
 			}
 		}
