f201df371cbbfee930f0d2a5e42ffc92cbd53eb3
digitaltomer <tomamadin@gmail.com>
2022-09-13 19:48:51 -0700

    101 natural

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -14,14 +14,14 @@ int main(void)
 	sum3 = 0;
 	sum = 0;
 
-	for (i = 0; i < 1024; ++1)
+	for (i = 0; i < 1024; ++i)
 	{
 		if ((i % 3) == 0)
 		{
 			sum3 = sum3 + i;
 		} else if ((i % 5) == 0)
 		{
-			sum5 = sum5 + 1;
+			sum5 = sum5 + i;
 		}
 	}
 	sum = sum3 + sum5;
