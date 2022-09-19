4adfe2fb021bd8321a879c62526c87191c241afb
digitaltomer <tomamadin@gmail.com>
2022-09-19 13:44:32 -0700

    strlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,6 +3,7 @@
 /**
  * _strlen - returns the length of a string
  * @s: string to define
+ * Return: the length of the string
  */
 
 int _strlen(char *s)
@@ -10,5 +11,6 @@ int _strlen(char *s)
 	int i;
 
 	for (i = 0; s[i] != '\0'; i++);
+
 	return (i);
 }
