8887c3eed53dbe04977ffed06f3fcf6dd4343f07
digitaltomer <tomamadin@gmail.com>
2022-09-19 15:17:30 -0700

    print rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse, 
+ * followed by a new line
+ * @s: prints a string, in reverse
+ */
+
+void print_rev(char *s)
+{
+	char str[] = i;
+
+	_putchar(strrev(str));
+
+	_putchar('\n');
+}
