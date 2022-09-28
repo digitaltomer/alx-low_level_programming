61be13f64621b9a6cb7d214812d48f2cf9ce879f
digitaltomer <tomamadin@gmail.com>
2022-09-27 19:08:17 -0700

    set strong

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - Sets the value of a pointer to char
+ * @s: the value toevaluate
+ * @to: the value to assign
+ * Return: (0) Success
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
