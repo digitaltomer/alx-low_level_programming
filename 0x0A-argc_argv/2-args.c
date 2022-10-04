d1bd3d407e680b1cb465ff82c2ecb47f9be4479a
digitaltomer <tomamadin@gmail.com>
2022-10-04 03:49:25 -0700

    2args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - program that prints all arguments it receives
+ * @argc: - argument count
+ * @argv: - argument vector
+ *
+ * Return: All arguments should be printed
+ */
+
+int main(int argc, char *argv[])
+{
+	int t;
+
+	for (t = 0; t < argc; t++)
+	{
+		printf("%s ", *argv[t]);
+	}
+	printf('\n');
+	return (0);
+}
