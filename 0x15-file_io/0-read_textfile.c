49639ba6ebddd820760c041371b5db4c974cea18
digitaltomer <tomamadin@gmail.com>
2022-11-01 15:51:20 -0700

    textfilec

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,35 +4,42 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include "main.h"
 
 /**
  * read_textfile - Reads a textfile and prints it to the POSIX standard output.
  * @filename: The filename.
  * @letters: The number of letters to read and print.
  * Return: The actual number of letters it could read and print if everything
- *	   runs well. 0 otherwise.
+ * runs well. 0 otherwise.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	int fd;
+	ssize_t lenr, lenw;
 	char *buffer;
-	int fd, tmp = 1;
-	size_t read_size = 0;
 
-	printf("la taille est %ld", letters);
-	if (!filename)
+	if (filename == NULL)
 		return (0);
 	fd = open(filename, O_RDONLY);
-	if(fd == -1)
+	if (fd == -1)
 		return (0);
-	buffer = malloc(letters * sizeof(char));
-	buffer[letters] = '\0';
-	do {
-		tmp = read(fd, buffer, letters);
-		read_size += tmp;
-	} while (tmp); 
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+	lenr = read(fd, buffer, letters);
 	close(fd);
-	if (read_size != letters)
+	if (lenr == -1)
+	{
+		free(buffer);
 		return (0);
-	write(1, buffer, letters);
-	return (read_size);
+	}
+	lenw = write(STDOUT_FILENO, buffer, lenr);
+	free(buffer);
+	if (lenr != lenw)
+		return (0);
+	return (lenw);
 }
